8331669b09fd7400dcf6144a318a7b3d5697d487
Bmorseletto <bmorseletto@fi.uba.ar>
2026-05-25 10:50:56 -0300

    fix: fixed eof sending

--- src/filter_date/main.py
+++ src/filter_date/main.py
@@ -9,7 +9,6 @@ from common import middleware, message_protocol
 
 ID = int(os.environ["ID"])
 MOM_HOST = os.environ["MOM_HOST"]
-OUTPUT_QUEUE = os.environ["OUTPUT_QUEUE"]
 FILTER_PREFIX = os.environ["FILTER_PREFIX"]
 OUTPUTS_PREFIX = os.environ["OUTPUTS_PREFIX"] # FORMATO: prefix1,prefix2,prefix (usamos split de python para hacer la lista de prefix)
 OUTPUTS_AMOUNTS = os.environ["OUTPUTS_AMOUNTS"]  # FORMATO: 1,2,3 (usamos split de python para hacer la lista de amounts)
@@ -25,6 +24,7 @@ class DateFilter:
         self.input_exchange = middleware.MessageMiddlewareExchangeRabbitMQ(
             MOM_HOST, FILTER_PREFIX, [f"{FILTER_PREFIX}",FILTER_PREFIX+f"{ID}"]
         )
+        logging.info(f"PERSONAL ROUTING KEY: {FILTER_PREFIX+f"{ID}"}")
         self.outputs_prefix = OUTPUTS_PREFIX.split(",")
         self.outputs_amounts = list(map(int,OUTPUTS_AMOUNTS.split(",")))
         self.routing_hash_targets = ROUTING_HASH_TARGET.split(",")
@@ -38,15 +38,19 @@ class DateFilter:
                     for j in range(self.outputs_amounts[i])
                 ],
             )
-            for i in range(len(OUTPUTS_PREFIX))
+            for i in range(len(self.outputs_prefix))
         ]
 
     def _process_data(self, transaction):
+        
         transaction_timestamp=datetime.strptime(transaction["timestamp"], "%Y/%m/%d %H:%M")
         initial_date = datetime.strptime(INITIAL_DATE, "%Y/%m/%d")
         end_date = datetime.strptime(END_DATE, "%Y/%m/%d")
+        logging.info(f"transaction_timestamp {transaction_timestamp}, initial_date {initial_date}, end_date {end_date} ")
+        logging.info(f"date comp: {initial_date <= transaction_timestamp <= end_date}")
+
         if initial_date <= transaction_timestamp <= end_date:
-            for i in len(self.output_exchanges):
+            for i in range(len(self.output_exchanges)):
                 routing_key = (
                 self.outputs_prefix[i]
                 + str(
@@ -64,7 +68,7 @@ class DateFilter:
         for i, output_exchange in enumerate(self.output_exchanges):
             output_exchange.send_by_key(
                 message_protocol.internal.serialize(
-                    {"nodo_id": ID, "client_id": deserialized_message[0]}
+                    {"nodo_id": ID, "client_id": deserialized_message["client_id"]}
                 ),
                 self.outputs_prefix[i],
             )
@@ -81,7 +85,7 @@ class DateFilter:
     def start(self):
         self.input_exchange.start_consuming(self.process_messsage)
         self.input_exchange.close()
-        for exchange in self.output_exchanges():
+        for exchange in self.output_exchanges:
             exchange.close()
 
     
